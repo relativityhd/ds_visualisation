ab254695e8f8d9404bf796f61854273926ab69b4
relativityhd <37540371+relativityhd@users.noreply.github.com>
2020-05-10 23:56:07 +0200

    Map Checkbox logic completed

--- Scripts/latency.js
+++ Scripts/latency.js
@@ -60,41 +60,60 @@
 
 // Input Radios sind mit Keywords versehen
 // Input Radios für Mapbox
-let airport_choice = "ankunft-port" // "ankunft-port" oder "abflug-port"
-let late_choice = "ankunft-late" // "ankunft-late" oder "abflug-late"
-let km_choice = "flugzeit" // "flugzeit" oder "aufgeholt"
+let airport_choice = "ankunft-port" // "ankunft-port" oder "abflug-port" [text]
+let late_choice = "ankunft-late" // "ankunft-late" oder "abflug-late" [größe]
+let km_choice = "flugzeit" // "flugzeit" oder "aufgeholt" [farbe]
 
 // Input Radios für Bar Plot
-let x_choice = "airline" // "airline", "flugnummer", "abflug-port" oder "ankunft-port"
-let y_choice = "flugzeit" // "flugzeit", "aufgeholt", "ankunft-late", "abflug-late"
+let x_choice = "airline" // "airline", "flugnummer", "abflug-port" oder "ankunft-port" [x-achse]
+let y_choice = "flugzeit" // "flugzeit", "aufgeholt", "ankunft-late", "abflug-late" [y-achse]
 
 let rows = []
 Plotly.d3.csv(
     // Ist noch nicht die locale file, weil ditt iwie nicht funktioniert aufgrund von Zugriffsbeschränkungen
     "https://gist.githubusercontent.com/florianeichin/b877d354d6bc52e6ce840572e40b0497/raw/19759410471073756a388dada5fcb40109f0d13e/flights_subset_cleaned.csv",
-	(err, r) => { rows = r }
+	(err, r) => {
+		rows = r
+		console.log(r)
+		renderMapPlot()
+	}
 );
 
 function unpack(rows, keyword) {
 
+	// Nach Flughafen Mappen
 	if (keyword=="ankunft-port") {
 		return rows.map((row) => {
 			return row["DESTINATION_AIRPORT"];
 		})
 	} else if (keyword=="abflug-port") {
-
+		return rows.map((row) => {
+			return row["ORIGIN_AIRPORT"];
+		})
 	} else if (keyword=="ankunft-late") {
-		
+		return rows.map((row) => {
+			return parseFloat(row["DESTINATION_DELAY"]);
+		})
 	} else if (keyword=="abflug-late") {
-		
+		return rows.map((row) => {
+			return parseFloat(row["DEPARTURE_DELAY"]);
+		})
 	} else if (keyword=="flugzeit") {
-		
+		return rows.map((row) => {
+			return parseFloat(row["SCHEDULED_TIME"] - row["ELAPSED_TIME"]) || 0;
+		})
 	} else if (keyword=="aufgeholt") {
-		
+		return rows.map((row) => {
+			return parseFloat(row["DESTINATION_DELAY"] - row["DEPARTURE_DELAY"])*(-1) || 0;
+		})
 	} else if (keyword=="airline") {
-		
+		return rows.map((row) => {
+			return row["AIRLINE"];
+		})
 	} else if (keyword=="flugnummer") {
-		
+		return rows.map((row) => {
+			return row["FLIGHT_NUMBER"];
+		})
 	}
 }
 
@@ -118,14 +137,35 @@ function unpack_pos(rows, keyword, lonlat) {
 	}
 }
 
-function map_radio_changed() {
+function renderMapPlot() {
+	// late_data ist ein Array bestehend aus den Values welche vom Benutzer gewählt wurden
+	let late_data = unpack(rows, late_choice),
+		minval = Math.min.apply(Math, late_data),
+		maxval = Math.max.apply(Math, late_data)
+	let size_data = late_data.map(val => {
+		val = val - minval
+		return parseInt(2 + (20*val/(maxval - minval)))
+	})
+
 	var data = [
 		{
 			type: "scattermapbox",
-			text: unpack(rows, "DESTINATION_AIRPORT"),
-			lon: unpack(rows, "DESTINATION_AIRPORT_LON"),
-			lat: unpack(rows, "DESTINATION_AIRPORT_LAT"),
-			marker: { color: "fuchsia", size: 4 }
+			text: unpack(rows, airport_choice),
+			lon: unpack_pos(rows, airport_choice, "lon"),
+			lat: unpack_pos(rows, airport_choice, "lat"),
+			marker: {
+				color: unpack(rows, km_choice),
+				colorscale: 'Portland',
+				showscale: true,
+				colorbar: {
+					title: {
+						text: (km_choice == "flugzeit" ? "Flugzeit" : "Aufgeholte Flugzeit")+" in Minuten",
+						side: "right"
+					}
+				},
+				cmin: minval,
+				cmax: maxval,
+				size: size_data }
 		}
 	];
 
@@ -135,5 +175,19 @@ function map_radio_changed() {
 		margin: { r: 0, t: 0, b: 0, l: 0 }
 	};
 
-	Plotly.newPlot("map-div", data, layout);
+	Plotly.newPlot("map-plot", data, layout);
+}
+
+function map_radio_changed(type, radioBtn) {
+
+	if (type=="port") {
+		airport_choice = radioBtn.value
+	} else if (type=="late") {
+		late_choice = radioBtn.value
+	} else if (type=="time") {
+		km_choice = radioBtn.value
+	} else {
+		return false
+	}
+	renderMapPlot()
 }
